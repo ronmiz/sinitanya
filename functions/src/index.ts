eb1bb3c7ec899fb4d6c879da8ae81a5de5e36242
ronmiz <ronmiz770@gmail.com>
2018-09-26 12:56:23 +0200

    functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,7 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+export const api = functions.https.onRequest((request, response) => {
+   let data = request.body
+    response.send(data);
+   });
